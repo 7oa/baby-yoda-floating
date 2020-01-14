e76730eb06ce74794f2330a764dc32c987a5b8c9
anna <semenova.7oa@yandex.ru>
2020-01-14 22:18:41 +0300

    press space to move

--- js/script.js
+++ js/script.js
@@ -7,21 +7,22 @@
   var ground = document.querySelector(".ground");
   var babyYoda = document.querySelector(".baby-yoda");
   var babyYodaShadow = document.querySelector(".baby-yoda-shadow");
+  var helpText = document.querySelector(".help-text");
 
   document.addEventListener("keydown", function(evt) {
     if (evt.keyCode === SPACE_KEYCODE) {
       ground.classList.add("animate");
       babyYoda.classList.add("float");
       babyYodaShadow.classList.add("float");
+      helpText.classList.remove("show");
     }
   });
 
   window.addEventListener("load", function() {
     babyYoda.classList.add("show");
     babyYodaShadow.classList.add("show");
-    // setTimeout(function() {
-    //   //babyYoda.classList.remove("show");
-    //   babyYoda.classList.add("float");
-    // }, 2100);
+    setTimeout(function() {
+      helpText.classList.add("show");
+    }, 1000);
   });
 })();
